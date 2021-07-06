5c1f29f5ba99322da3ce334c6276c5dbdc5a7545
DmitryCAP <64603201+DmitryCAP@users.noreply.github.com>
2021-07-06 22:12:09 +0700

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./../App.css";
+
+const Header = () => {
+    return(
+    <div className="header">
+        <header>
+          <div>
+            <img
+              src="https://cdn.postupi.online/images/images823/05/528.jpg"
+              alt="load"
+            />
+            <a>Develop technologies together!</a>
+          </div>
+        </header>
+      </div>)
+};
+
+export default Header;
---
+++ Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./../App.css";
+
+const Profile = () => {
+    return(
+    <div className="content">
+        <div>
+          <img
+            src="https://img.zoneofgames.ru/news/2018/12/03/131729-4ww9ag6ql61z.jpg"
+            alt="load"
+          />
+        </div>
+        <div>AVA+DESCRIPTION</div>
+        <div>Posts</div>
+        <div>My posts</div>
+        <div>New post</div>
+        <div> post 1</div>
+        <div> post 2</div>
+      </div>)
+};
+export default Profile;
